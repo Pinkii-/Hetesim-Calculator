da1970efa01788e5fb4c3da5a22fb17e50153a76
fndh <xavi_cat36@hotmail.com>
2016-04-20 22:16:51 +0200

    Nodepaircomparatorfixedomgquepalo

--- eclipseProject/src/Dominio/NodePairComparator.java
+++ eclipseProject/src/Dominio/NodePairComparator.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 //Used by Result.java to sort its NodePairs based on their Hetesim values
 public class NodePairComparator implements Comparator<NodePair>{
 	public int compare(NodePair n1, NodePair n2){
-		return ((int)n1.getHetesim() - (int)n2.getHetesim());
+		return (n1.getHetesim() < n2.getHetesim() ? 1 : -1);
 	}
 }
 
