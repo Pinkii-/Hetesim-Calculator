a504a6a7f9165515eee7d0d0633594d42cb1550f
fndh <xavi_cat36@hotmail.com>
2016-04-23 13:56:57 +0200

    Much comment. Very /**

--- eclipseProject/src/Dominio/Result.java
+++ eclipseProject/src/Dominio/Result.java
@@ -1,10 +1,13 @@
 package Dominio;
 
+
+/*
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+*/
 import java.io.Serializable;
 
 import java.util.ArrayList;
@@ -12,18 +15,17 @@ import java.util.Collections;
 
 
 /**
- * 
  * @author Xavier
+ * 
+ * This class stores the relation values from the HeteSim in an ArrayList made up of NodePairs.
+ * given two Nodes and a Float. It allows the modification of the HeteSim values for a NodePair,
+ * or its complete deletion.
+ * Given more than one NodePair, this class will sort them in a descending order based on their value.
+ *
  * 
  */
 
 @SuppressWarnings("serial")
-/*
- * If you're actually using serialization, it only matters if you plan on storing and retrieving objects using serialization directly.
- * The serialVersionUID represents your class version, and you should increment it if the current version of your class is not backwards
- * compatible with its previous version.
- * 
- */
 public class Result implements Cloneable, Serializable{
 	private Node firstN; //Search origin node
 	private Node lastN; //Search destination node
@@ -39,9 +41,17 @@ public class Result implements Cloneable, Serializable{
 	private ArrayList<NodePair> resultList; //Hetesim results, structured in NodePairs and (hopefully) sorted by Hetesim values
 	
 	
-	//Only one path
+	/**
+	 * Constructor for the case in which the Hetesim works with only one Path.
+	 * 
+	 * 
+	 * @param g > Graph used to get the Node objects. 
+	 * @param threshold > Float in range [0..1]. Will be used to display the result from this value.
+	 * @param resultHete > Matrix with the hetesim values corresponding to the nodes i, j.
+	 * @param p > Path used to calculate the hetesim values.
+	 */
 	public Result(final Graf g, final Float threshold, final Matrix resultHete, final Path p){
-		assert (resultHete != null);
+		if (resultHete == null) throw new RuntimeException("Null Matrix. Can't create result.");
 		
 		usedP = p;
 		idResult = new String(g.getNom() + " " + p.toString());
@@ -64,10 +74,22 @@ public class Result implements Cloneable, Serializable{
 		
 		sortResult(); //Sort result list
 	}
+	
+	
+	/**
+	 * Constructor for the case in which the Hetesim works with one Node and one Path.
+	 * 
+	 * 
+	 * @param g > Graph used to get the Node objects. 
+	 * @param threshold  > Float in range [0..1]. Will be used to display the result from this value.
+	 * @param resultHete > ArrayList with the HeteSim values corresponding to the node n1. Organized in pairs with the destination node and their hetesim value.
+	 * @param p > Path used to calculate the HeteSim values.
+	 * @param n1 > Starting node from which HeteSim was calculated.
+	 */
 	//One node, one path
 	public Result(final Graf g, final Float threshold, final ArrayList<Pair<Integer,Float>> resultHete, final Path p, final Node n1) {
 		assert (p.getContingut().get(0) == n1.getTipus());
-		assert (resultHete != null);
+		if (resultHete == null) throw new RuntimeException("Null ArrayList. Can't create result.");
 		
 		firstN = n1;
 		usedP = p;
@@ -83,12 +105,24 @@ public class Result implements Cloneable, Serializable{
 		
 		sortResult(); //Sort result list
 	}
-	//Two nodes, one path
+	
+	
+	/**
+	 * Constructor for the case in which the HeteSim works with two Nodes and one Path.
+	 * 
+	 * 
+	 * @param g > Graph used to get the Node objects. 
+	 * @param threshold  > Float in range [0..1]. Will be used to display the result from this value.
+	 * @param resultHete > Float with the HeteSim value corresponding to the node n1 and n2 with path p.
+	 * @param p > Path used to calculate the HeteSim values.
+	 * @param n1 > Starting node from which HeteSim was calculated.
+	 * @param n2 > Destination node from which HeteSim was calculated.
+	 */
 	public Result(final Graf g, final float threshold, final Float resultHete, final Path p, final Node n1, final Node n2){
 		//Assert that the path starts with the node type N1 and ends with the node type N2
 		assert (p.getContingut().get(0) == n1.getTipus());
 		assert (p.getContingut().get(p.getLength()-1) == n2.getTipus());
-		assert (resultHete != null);
+		if (resultHete == null) throw new RuntimeException("Null Float. Can't create result.");
 		
 		firstN = n1;
 		lastN = n2;
@@ -121,12 +155,16 @@ public class Result implements Cloneable, Serializable{
 		return retStr; 
 	}
 
-	/* 
-	 * Checks consistency with the loaded graph. If the ids don't match,
-	 * the return value of toString will have an extra line with "Not consistent"
+
+	/**
+	 * Checks consistency with the loaded graph. If the ids don't match, the
+	 * return value of toString will have an extra line with "Not consistent"
 	 * at the beggining.
 	 * 
-	*/
+	 * 
+	 * @param g > Graph used to check consistency with the stored nodes.
+	 * @return String with a value of result.toString(). It might have an extra line at the start. 
+	 */
 	public String toString(Graf g){
 		String retStr = new String();
 		if (String.valueOf(g.id) != idGraph || modified) retStr = retStr + "Not consistent!\n";
@@ -134,8 +172,12 @@ public class Result implements Cloneable, Serializable{
 		return retStr;
 	}
 	
-	
-	//Set new threshold. When calling toString(), only the NodePairs with Hetesim value over this threshold will be displayed.
+	/**
+	 * Set new threshold. When calling toString(), only the NodePairs with Hetesim value over this threshold will be displayed.
+	 * 
+	 * 
+	 * @param threshold > Used to define a new value from which Result will be displayed.
+	 */
 	public void setThreshold(float threshold){
 		this.threshold = threshold;
 	}
@@ -153,6 +195,7 @@ public class Result implements Cloneable, Serializable{
 	
 	
 	//Clone all the information, not just the main object
+	/*
 	public Result deepClone() {
 		try {
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -168,7 +211,7 @@ public class Result implements Cloneable, Serializable{
 			return null;
 		}
 	}
-	
+	*/
 	
 	
 	public String getIdResult(){
@@ -188,24 +231,39 @@ public class Result implements Cloneable, Serializable{
 		this.idGraph = idGraph;
 	}
 	
-	//Modify the value of the heteSim for the line i
+	
+	/**
+	 * Modify the value of the HeteSim for the NodePair stored in line i
+	 * 
+	 * 
+	 * @param i > Indicates which result line should have its HeteSim value modified.
+	 * @param hetesimVal > The new modified value for the NodePair.
+	 */
 	public void modifLine(Integer i, Float hetesimVal){
-		assert (i < resultList.size());
+		if (i < 0 || i > resultList.size()) throw new ArrayIndexOutOfBoundsException("Can't access the element " + i + " in resultList of size " + resultList.size() + ".");
 		
 		resultList.get(i).setHetesim(hetesimVal);
 		modified = true;
 		sortResult();
 	}
 	
-	//Delete the line i
+	/**
+	 * Delete the NodePair stored in the line i
+	 * 
+	 * 
+	 * @param i > Indicates which result line should be removed.
+	 */
 	public void deleteLine(Integer i){
-		assert (i < resultList.size());
+		if (i < 0 || i > resultList.size()) throw new ArrayIndexOutOfBoundsException("Can't access the element " + i + " in resultList of size " + resultList.size() + ".");
 		
 		resultList.remove(i);
 		modified = true;
 	}
 	
-	//If either the result or the graph have been modified, the information is not consistent and toString() will issue a warning
+	/**
+	 * If either the result or the graph have been modified, the information is not consistent and toString() will issue a warning
+	 * 
+	 */
 	public void setModified(){ 
 		modified = true;
 	}
