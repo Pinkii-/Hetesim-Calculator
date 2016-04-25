6aee105b9da9743e355d60a3d4a200aa9e5c6bfc
Pinkii- <pinkiibiza@gmail.com>
2016-04-25 15:43:06 +0200

    no null

--- eclipseProject/src/Dominio/CtrlSearch.java
+++ eclipseProject/src/Dominio/CtrlSearch.java
@@ -17,29 +17,29 @@ public class CtrlSearch {
 	}
 
 	public Result searchPathThreshhold(final Graf g, final Float threshold, final Path p) throws PathException {
-		return new Result(g, threshold, het.getHeteSim(p), p);
+		return new Result(this.g, threshold, het.getHeteSim(p), p);
 	}
 
 	public Result searchPath(final Graf g, final Path p) throws PathException {
-		return searchPathThreshhold(g, 0.f, p);
+		return searchPathThreshhold(this.g, 0.f, p);
 	}
 
 	public Result searchPathNodeThreshhold(final Graf g, final Float threshold, final Path p, final Node n)
 			throws PathException {
-		return new Result(g, threshold, het.getHeteSim(p, n), p, n);
+		return new Result(this.g, threshold, het.getHeteSim(p, n), p, n);
 	}
 
 	public Result searchPathNode(final Graf g, final Path p, final Node n) throws PathException {
-		return searchPathNodeThreshhold(g, 0.f, p, n);
+		return searchPathNodeThreshhold(this.g, 0.f, p, n);
 	}
 
 	public Result searchPathNodeNodeThreshhold(final Graf g, final Float threshold, final Path p, final Node n1,
 			final Node n2) throws PathException {
-		return new Result(g, threshold, het.getHeteSim(p, n1, n2), p, n1, n2);
+		return new Result(this.g, threshold, het.getHeteSim(p, n1, n2), p, n1, n2);
 	}
 
 	public Result searchPathNodeNode(final Graf g, final Path p, final Node n1, final Node n2) throws PathException {
-		return searchPathNodeNodeThreshhold(g, 0.f, p, n1, n2);
+		return searchPathNodeNodeThreshhold(this.g, 0.f, p, n1, n2);
 	}
 
 }
