e1dbb273e6d28cd673e714ae30c82e01f9b6d8a6
Pinkii- <pinkiibiza@gmail.com>
2016-05-11 16:10:45 +0200

    Prensentacion(?)

---
+++ eclipseProject/src/Presentacion/CtrlPresentacion.java
@@ -0,0 +1,12 @@
+package Presentacion;
+
+import Dominio.CtrlDominio;
+
+public class CtrlPresentacion {
+	
+	CtrlDominio ctrlDominio = new CtrlDominio();
+	VistaPrincipal vp = new VistaPrincipal();
+	
+	
+	
+}
