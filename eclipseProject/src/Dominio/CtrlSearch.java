a0f8e8f120ce3f346adaffca0d5cb44933db08ce
Pinkii- <pinkiibiza@gmail.com>
2016-05-13 12:06:48 +0200

    no nesisito el graph

--- eclipseProject/src/Dominio/CtrlSearch.java
+++ eclipseProject/src/Dominio/CtrlSearch.java
@@ -16,32 +16,31 @@ public class CtrlSearch {
 		het.setGraph(g);
 	}
 
-	public Result searchPathThreshhold(final Graph g, final Float threshold, final Path p) throws PathException {
+	public Result searchPathThreshhold(final Float threshold, final Path p) throws PathException {
 		return new Result(this.g, threshold, het.getHeteSim(p), p);
 	}
 
-	public Result searchPath(final Graph g, final Path p) throws PathException {
-		return searchPathThreshhold(g, 0.f, p);
+	public Result searchPath(final Path p) throws PathException {
+		return searchPathThreshhold(0.f, p);
 	}
 
-	public Result searchPathNodeThreshhold(final Graph g, final Float threshold, final Path p, final Node n)
+	public Result searchPathNodeThreshhold(final Float threshold, final Path p, final Node n)
 			throws PathException {
 		return new Result(this.g, threshold, het.getHeteSim(p, n), p, n);
 	}
 
 
-	public Result searchPathNode(final Graph g, final Path p, final Node n) throws PathException {
-		return searchPathNodeThreshhold(this.g, 0.f, p, n);
+	public Result searchPathNode(final Path p, final Node n) throws PathException {
+		return searchPathNodeThreshhold(0.f, p, n);
 	}
 
-	public Result searchPathNodeNodeThreshhold(final Graph g, final Float threshold, final Path p, final Node n1,
-			final Node n2) throws PathException {
+	public Result searchPathNodeNodeThreshhold(final Float threshold, final Path p, final Node n1,final Node n2) throws PathException {
 		return new Result(this.g, threshold, het.getHeteSim(p, n1, n2), p, n1, n2);
 	}
 
 
-	public Result searchPathNodeNode(final Graph g, final Path p, final Node n1, final Node n2) throws PathException {
-		return searchPathNodeNodeThreshhold(this.g, 0.f, p, n1, n2);
+	public Result searchPathNodeNode(final Path p, final Node n1, final Node n2) throws PathException {
+		return searchPathNodeNodeThreshhold(0.f, p, n1, n2);
 	}
 
 }
