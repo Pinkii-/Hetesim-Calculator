912e75324b98aa5bb69b982ae73cadcf74805831
fndh <xavi_cat36@hotmail.com>
2016-05-25 14:28:18 +0200

    Fixed stuff

---
+++ eclipseProject/src/Dominio/formatedNodeComparator.java
@@ -0,0 +1,10 @@
+package Dominio;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class formatedNodeComparator implements Comparator<ArrayList<String>>{
+	public int compare(ArrayList<String> a1, ArrayList<String> a2){
+		return a1.get(1).compareTo(a2.get(1));
+	}
+}
