b8ea72d58c65df4e16d821ca8f7f39ba3116a4d7
wextia <valcazar96@gmail.com>
2016-05-31 16:50:03 +0200

    NUCLEAR LAUNCH DETECTED

--- eclipseProject/src/Propject.java
+++ eclipseProject/src/Propject.java
@@ -5,43 +5,10 @@ import Presentacion.CtrlPresentacion;
 public class Propject {
 
 	public static void main(String[] args) {
-		System.out.println("HelloOoOoO PROP");
-		
+		System.out.println("RUSTLED PROP RESPONSIVE EDITION... booting...");
+		System.err.println("Tecsto rojo");	
+		@SuppressWarnings("unused")
 		CtrlPresentacion cp = new CtrlPresentacion();
-		
-		
-		// Legacyda
-//		Graph g = GraphImporter.leMagicGoesOn("/home/pinkii/Documents/PROP/DBLP_four_area/");
-//		
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		
-//		CtrlSearch cs = new CtrlSearch();
-//
-//		System.out.println("Start Hetesim ?");
-//		br.readLine();
-//		System.out.println("Starting Hetesim");
-//		
-//		Path p = new Path();
-//		p.setContingut("APA");
-//		
-//		System.out.println(g.getNode(7695, Node.Type.Autor).getNom() + " " + g.getNode(416, Node.Type.Terme).getNom());
-//		
-////		g.getNode(7696, Node.Type.Autor),g.getNode(417, Node.Type.Terme)
-//		
-////		SparseMatrix ssm = h.getHeteSim(p);
-//		
-////		System.out.println("Hetsim done " + ssm.getValue(7695,416));
-//		
-////		System.out.println(g.getMatrixTerm().getRow(416));
-////		g.getMatrixTerm().transpose();
-//		
-//		cs.setGraph(g);
-//		
-//		System.out.println("hetesim " + cs.searchPathNodeThreshhold(g, 0.1f, p, g.getNode(7695, Node.Type.Autor)));
-//		
-//		br.readLine();
-//		
-////		System.out.println(ssm);
 	}
 
 }
